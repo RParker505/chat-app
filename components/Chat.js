2ed29a1d9bdc84be8315e17b653b39f494efda46
RParker505 <rchiado75@gmail.com>
2024-06-10 06:13:16 -0600

    Add conditional for KeyboardAvoidingView for Androids

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
 import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({route, navigation}) => {
@@ -33,21 +33,23 @@ useEffect(() => {
 }, []);
 
  return (
-  <GiftedChat
-    messages={messages}
-    onSend={messages => onSend(messages)}
-    user={{
-      _id: 1
-    }}
-  />
+  <View style={[styles.container, { backgroundColor: background }]}>
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1
+      }}
+    />
+    { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null } 
+    {/* if OS is Android, add KeyboardAvoidingView */}
+  </View>
   )
 };
 
 const styles = StyleSheet.create({
  container: {
    flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center'
  }
 });
 
