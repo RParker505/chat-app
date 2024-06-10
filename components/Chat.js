b85798ac2ce592cb3c68729d89e16852a7681f4d
RParker505 <rchiado75@gmail.com>
2024-06-10 17:17:53 -0600

    Adjust colors of message bubbles and add a system message

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
-import { GiftedChat } from "react-native-gifted-chat";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 const Chat = ({route, navigation}) => {
 
@@ -10,6 +10,21 @@ const [messages, setMessages] = useState([]);
 // Called when a user sends a message, appends new message to newMessages array, which is then appended to original list of messages
 const onSend = (newMessages) => {
   setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+}
+
+// change default colors of sender and receiver bubbles
+const renderBubble = (props) => {
+  return <Bubble
+    {...props}
+    wrapperStyle={{
+      right: {
+        backgroundColor: "#000"
+      },
+      left: {
+        backgroundColor: "#FFF"
+      }
+    }}
+  />
 }
 
 //  Called right after the Chat component mounts
@@ -25,6 +40,12 @@ const onSend = (newMessages) => {
         avatar: "https://placeimg.com/140/140/any",
       },
     },
+    {
+      _id: 2,
+      text: 'This is a system message',
+      createdAt: new Date(),
+      system: true,
+    },
   ]);
 }, []);
 
@@ -36,6 +57,7 @@ useEffect(() => {
   <View style={[styles.container, { backgroundColor: background }]}>
     <GiftedChat
       messages={messages}
+      renderBubble={renderBubble}
       onSend={messages => onSend(messages)}
       user={{
         _id: 1
