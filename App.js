02949686bafe8b0fdc6761aa1bac9db03b2ae246
RParker505 <rchiado75@gmail.com>
2024-06-17 16:14:17 -0600

    Initialize Firebase and Firestore and pass db as a prop to Chat component

--- App.js
+++ App.js
@@ -5,11 +5,29 @@ import Chat from './components/Chat';
 // import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAwPGvKPAAWqlB23LF4M37IJOgZUQPrq5c",
+    authDomain: "chat-app-afe2e.firebaseapp.com",
+    projectId: "chat-app-afe2e",
+    storageBucket: "chat-app-afe2e.appspot.com",
+    messagingSenderId: "23687966628",
+    appId: "1:23687966628:web:11931e11729176624cdb31"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -21,8 +39,9 @@ const App = () => {
         />
         <Stack.Screen
           name='Chat'
-          component={Chat}
-        />
+        >
+          {props => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
