70f3affd76852d561897d4f3887c1db928a89859
RParker505 <rchiado75@gmail.com>
2024-06-20 05:12:29 -0600

    Import Alert, disableNetwork and enableNetwork

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Alert } from 'react-native';
 // import the screens
 import Start from './components/Start';
 import Chat from './components/Chat';
@@ -6,7 +6,7 @@ import Chat from './components/Chat';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, disableNetwork, enableNetwork } from "firebase/firestore";
 
 // Create the navigator
 const Stack = createNativeStackNavigator();
