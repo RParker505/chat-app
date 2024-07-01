4ac8e102da367f5fb1c8a210e438bcd6e48e2d78
RParker505 <rchiado75@gmail.com>
2024-07-01 16:29:06 -0600

    Create shell of CustomActions component

---
+++ components/CustomActions.js
@@ -0,0 +1,14 @@
+import { TouchableOpacity } from "react-native";
+
+const CustomActions = () => {
+
+    const onActionPress = () => {}
+
+    return (
+        <TouchableOpacity style={styles.container} onPress={onActionPress}> 
+        </TouchableOpacity>
+      );
+
+}
+
+export default CustomActions;
