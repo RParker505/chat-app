5ebba2ddcc58412b0a41603c0e52a88015edb663
RParker505 <rchiado75@gmail.com>
2024-07-01 16:34:23 -0600

    Set styles for wrapper, iconText

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,6 +1,6 @@
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
 
-const CustomActions = () => {
+const CustomActions = ({wrapperStyle, iconTextStyle}) => {
 
     const onActionPress = () => {}
 
@@ -13,5 +13,27 @@ const CustomActions = () => {
       );
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+      width: 26,
+      height: 26,
+      marginLeft: 10,
+      marginBottom: 10,
+    },
+    wrapper: {
+      borderRadius: 13,
+      borderColor: '#b2b2b2',
+      borderWidth: 2,
+      flex: 1,
+    },
+    iconText: {
+      color: '#b2b2b2',
+      fontWeight: 'bold',
+      fontSize: 10,
+      backgroundColor: 'transparent',
+      textAlign: 'center',
+    },
+  });
 
 export default CustomActions;
