0b5b711c51639e418faae452b71965f5fb41650f
RParker505 <rchiado75@gmail.com>
2024-07-02 05:11:24 -0600

    Add renderCustomView as a prop in GiftedChat and write function for it

--- components/Chat.js
+++ components/Chat.js
@@ -143,6 +143,27 @@ const renderCustomActions = (props) => {
   return <CustomActions {...props} />;
 };
 
+const renderCustomView = (props) => {
+  const { currentMessage} = props;
+  if (currentMessage.location) {
+    return (
+        <MapView
+          style={{width: 150,
+            height: 100,
+            borderRadius: 13,
+            margin: 3}}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+    );
+  }
+  return null;
+}
+
  return (
   <View style={[styles.container, { backgroundColor: background }]}>
     <GiftedChat
@@ -151,6 +172,7 @@ const renderCustomActions = (props) => {
       renderInputToolbar={renderInputToolbar}
       renderSystemMessage={renderSystemMessage}
       renderActions={renderCustomActions}
+      renderCustomView={renderCustomView}
       onSend={messages => onSend(messages)}
       user={{
         _id: userID,
