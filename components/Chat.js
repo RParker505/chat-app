cd84d8e3ac02ac01c411279ead36a2c03867313b
RParker505 <rchiado75@gmail.com>
2024-07-02 06:07:39 -0600

    Extract storage prop

--- components/Chat.js
+++ components/Chat.js
@@ -6,7 +6,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import CustomActions from './CustomActions';
 import MapView from 'react-native-maps';
 
-const Chat = ({route, navigation, db, isConnected}) => {
+const Chat = ({route, navigation, db, isConnected, storage}) => {
 
 const { name, background, userID } = route.params;
 const [messages, setMessages] = useState([]);
